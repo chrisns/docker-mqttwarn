6149a328d398619b1424994b590c07f1d034dc22
Chris Nesbitt-Smith <chris@cns.me.uk>
2017-08-22 13:41:27 +0100

    allow changes to this repo to rebuild the docker image

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ function docker_tag_exists() {
     curl --silent -f -lSL https://index.docker.io/v1/repositories/$1/tags/$2 > /dev/null
 }
 
-if docker_tag_exists chrisns/mqttwarn ${VERSION}; then
+if docker_tag_exists chrisns/mqttwarn ${VERSION} || [ "${TRAVIS_EVENT_TYPE}" != "cron" ]; then
   echo ${VERSION} already exists
 else
   echo ${VERSION} does not yet exist
